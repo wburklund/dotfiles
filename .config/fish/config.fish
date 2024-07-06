6abfa28101347cb7ba22079ba59f0290b5739c70
William Burklund <waburklund@gmail.com>
2024-07-06 02:29:11 -0400

    Move to brew bundle, get rid of brew-file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,3 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-if test -f (brew --prefix)/etc/brew-wrap.fish
-  source (brew --prefix)/etc/brew-wrap.fish
-end
-
