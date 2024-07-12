e0a76d81f8f284540f5df33cdf978246a96c329e
William Burklund <waburklund@gmail.com>
2024-07-11 21:24:13 -0400

    Let VimTeX handle LaTeX highlighting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1017,6 +1017,7 @@ require('lazy').setup({
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        disable = { 'latex' },
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
