44949cb4d4ed59c8425c6429b162f9df5aa30fa5
William Burklund <waburklund@gmail.com>
2024-07-14 00:34:49 -0400

    Use global status line

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -987,6 +987,9 @@ require('lazy').setup({
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
+      options = {
+        globalstatus = true,
+      },
       sections = {
         lualine_c = {},
         lualine_x = {},
