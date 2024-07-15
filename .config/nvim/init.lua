0fb6326b8b73038cfbc8c9473ed1266437046b0c
Will Burklund <will.burklund@wgu.edu>
2024-07-15 15:27:35 -0400

    Remove unnecessary paste keybinds (P/<C-R>+

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -189,12 +189,6 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- Paste
-vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
-vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
-vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
-vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
-
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
