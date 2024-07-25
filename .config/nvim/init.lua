7a728b210b3feeeecc846754289ae7817741a5c0
Will Burklund <will.burklund@wgu.edu>
2024-07-25 17:52:05 -0400

    Add Prettier to Neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -953,7 +953,7 @@ ORDER BY position;]]
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
         return {
-          timeout_ms = 500,
+          timeout_ms = 1000,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
         }
       end,
@@ -966,6 +966,11 @@ ORDER BY position;]]
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
+        html = { 'prettierd' },
+        css = { 'prettierd' },
+        javascript = { 'prettierd' },
+        json = { 'prettierd' },
+        yaml = { 'prettierd' },
       },
       formatters = {
         ruff_format = {
