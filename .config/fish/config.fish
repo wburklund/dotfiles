641434d3b9aaa2a6645a5496fe4f844cb91c0438
William Burklund <waburklund@gmail.com>
2026-04-12 15:08:31 -0400

    Update Fish, Neovim plugins

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,14 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+if test -e ~/.config/fish/fish_variables.local
+    source ~/.config/fish/fish_variables.local
+end
+
 set -gx HOMEBREW_BUNDLE_FILE ~/.config/brewfile/Brewfile
 set -gx AWS_CLI_AUTO_PROMPT on
 set -gx AWS_PAGER
 set -gx AWS_DEFAULT_OUTPUT table
 set -gxa fish_user_paths $(realpath ~/.deno/bin)
 
+set PATH $PATH ~/.local/bin
